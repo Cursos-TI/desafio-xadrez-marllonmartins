cb77c9fcadfe13994a526c1421c02edd3e81fd49
marllonmartins <paplays40@gmail.com>
2025-06-06 18:54:35 +0000

    nivel aventureiro

--- xadrez.c
+++ xadrez.c
@@ -2,19 +2,18 @@
 
 int main() {
 
+    int peca, lado, b = 0 , t , r = 0, c1, c2 = 0;
 
-    //Variaveis usadas
-    int peca, lado, b = 0 , t , r = 0;
-    //Escolha da peça que sera mexida
     printf("Escolha a peça que deseja mexer...\n");
     printf("1 = Torre\n");
     printf("2 = Bispo\n");
     printf("3 = Rainha\n");
+    printf("4 = Cavalo\n");
     scanf("%d", &peca);
 
     switch (peca)
     {
-    case 1:// escolha para qual lado a torre ira mexer
+    case 1:
         printf("Escolha para qual lado...\n");
         printf("1 = Frente\n");
         printf("2 = Trás\n");
@@ -52,7 +51,7 @@ int main() {
 
         break;
 
-    case 2://Escolha para qual lado o bispo ira mexer
+    case 2:
         printf("Escolha pra qual lado...\n");
         printf("1 = Diagonal para frente e para a esquerda\n");
         printf("2 = Diagonal para frente e para a direita\n");
@@ -95,7 +94,7 @@ int main() {
         
         break;
 
-    case 3://Escolha pra qual lado a rainha ira mexer
+    case 3:
         printf("Escolha pra qual lado...\n");
         printf("1 = Diagonal para esquerda\n");
         printf("2 = Diagonal para direita\n");
@@ -104,35 +103,36 @@ int main() {
         printf("5 = Para frente\n");
         printf("6 = Para tras\n");
         scanf("%d", &lado);
+        printf("\n");
 
         r = 0;
         if (lado == 1) {
-            while (r < 5) {
+            while (r < 8) {
                 printf("Um para frente e um para esquerda\n");
                 r++;
             }
         } else if (lado == 2) {
-            while (r < 5) {
+            while (r < 8) {
                 printf("Um para frente e um para direita\n");
                 r++;
             }
         } else if (lado == 3) {
-            while (r < 5) {
+            while (r < 8) {
                 printf("Esquerda\n");
                 r++;
             }
         } else if (lado == 4) {
-            while (r < 5) {
+            while (r < 8) {
                 printf("Direita\n");
                 r++;
             }
         } else if (lado == 5) {
-            while (r < 5) {
+            while (r < 8) {
                 printf("Para frente\n");
                 r++;
             }
         } else if (lado == 6) {
-            while (r < 5) {
+            while (r < 8) {
                 printf("Para trás\n");
                 r++;
             }
@@ -140,7 +140,75 @@ int main() {
             printf("Lado inválido!\n");
         }
         break;
-        default;//caso a nenhuma peca dessa 3 for escolhida
+case 4:
+    printf("Escolha pra qual lado...\n");
+    printf("1 = L para frente e para direita\n");
+    printf("2 = L para frente e para esquerda\n");
+    printf("3 = L para trás e para direita\n");
+    printf("4 = L para trás e para esquerda\n");
+    scanf("%d", &lado);
+
+    if (lado == 1) {
+        for (c1 = 1; c1 <= 1; c1++) {
+            printf("Frente\n");
+            printf("Frente\n");
+
+            c2 = 0;
+            while (c2 < 1) {
+                printf("Direita\n");
+                c2++;
+                printf("\n");
+            }
+        }
+
+    } else if (lado == 2) {
+        for (c1 = 1; c1 <= 1; c1++) {
+            printf("Frente\n");
+            printf("Frente\n");
+
+            c2 = 0;
+            while (c2 < 1) {
+                printf("Esquerda\n");
+                c2++;
+                printf("\n");
+            }
+        }
+
+    } else if (lado == 3) {
+        for (c1 = 1; c1 <= 1; c1++) {
+            printf("Trás\n");
+            printf("Trás\n");
+
+            c2 = 0;
+            while (c2 < 1) {
+                printf("Direita\n");
+                c2++;
+                printf("\n");
+            }
+        }
+
+    } else if (lado == 4) {
+        for (c1 = 1; c1 <= 1; c1++) {
+            printf("Trás\n");
+            printf("Trás\n");
+
+            c2 = 0;
+            while (c2 < 1) {
+                printf("Esquerda\n");
+                c2++;
+                printf("\n");
+            }
+        }
+    } else{
+
+            printf("Lado invalido\n");
+    }
+
+    break;
+   
+            
+        
+    default:
         printf("Peça não implementada.\n");
         break;
     }
