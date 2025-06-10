9d600dba4fdfc8d9dc1430c99c7d743429d6ab07
marllonmartins <paplays40@gmail.com>
2025-06-10 17:55:44 +0000

    Nivel Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,217 +1,124 @@
-#include<stdio.h>
+
+#include <stdio.h>
+
+#define CASAS_TORRE 5
+#define CASAS_BISPO 5
+#define CASAS_RAINHA 8
+
+// Torre recursiva
+void moverTorre(char* direcao, int casas) {
+    if (casas == 0) return;
+    printf("%s\n", direcao);
+    moverTorre(direcao, casas - 1);
+}
+
+// Rainha recursiva
+void moverRainha(char* direcao, int casas) {
+    if (casas == 0) return;
+    printf("%s\n", direcao);
+    moverRainha(direcao, casas - 1);
+}
+
+// Bispo recursiva
+void moverBispoRec(char* direcao, int casas) {
+    if (casas == 0) return;
+    printf("%s\n", direcao);
+    moverBispoRec(direcao, casas - 1);
+}
+
+// Bispo loops aninhados
+void moverBispoComLoops() {
+    printf("\nMovimento do Bispo com loops aninhados:\n");
+    for (int i = 0; i < 2; i++) { // movimento vertical
+        for (int j = 0; j < 2; j++) { // movimento horizontal
+            printf("Diagonal (Vertical %d, Horizontal %d)\n", i + 1, j + 1);
+        }
+    }
+}
+
+// Cavalo loops complexos 
+void moverCavalo() {
+    printf("\nMovimento do Cavalo em 'L' para cima e direita:\n");
+    int movimento_feito = 0;
+    for (int i = 0; i < 3 && !movimento_feito; i++) {
+        for (int j = 0; j < 3 && !movimento_feito; j++) {
+            if (i == 2 && j == 1) {
+                printf("Cima\n");
+                printf("Cima\n");
+                printf("Direita\n");
+                movimento_feito = 1;
+                break;
+            }
+        }
+    }
+}
 
 int main() {
-
-    int peca, lado, b = 0 , t , r = 0, c1, c2 = 0;
+    int peca, lado;
 
     printf("Escolha a peça que deseja mexer...\n");
-    printf("1 = Torre\n");
-    printf("2 = Bispo\n");
-    printf("3 = Rainha\n");
-    printf("4 = Cavalo\n");
+    printf("1 = Torre\n2 = Bispo\n3 = Rainha\n4 = Cavalo\n");
     scanf("%d", &peca);
 
-    switch (peca)
-    {
-    case 1:
-        printf("Escolha para qual lado...\n");
-        printf("1 = Frente\n");
-        printf("2 = Trás\n");
-        printf("3 = Direita\n");
+    switch (peca) {
+    case 1: // torre
+        printf("Escolha o lado:\n");
+        printf("1 = Cima\n");
+        printf("2 = Baixo\n");
+        printf("3 =Direita\n");
         printf("4 = Esquerda\n");
-        scanf("%d", &lado);
-
-        if (lado == 1) {
-            for (t = 0; t < 5; t++) {
-                printf("Para frente\n");
-            }
-        } else if (lado == 2)
-      
-        {
-            for (t = 0; t < 5; t++) {
-                printf("Para trás\n");
-            }
-        } else if (lado == 3)
-        {
-            for (t = 0; t < 5; t++)
-            {
-                printf("Para a Direita\n");
-            }
-            
-        } else
-        {
-            for (t = 0; t < 5; t++)
-            {
-                printf("Para a Esquerda\n");
-            }
-            
-        }
-        
-        
-
-        break;
-
-    case 2:
-        printf("Escolha pra qual lado...\n");
-        printf("1 = Diagonal para frente e para a esquerda\n");
-        printf("2 = Diagonal para frente e para a direita\n");
-        printf("3 = Diagonal para trás e para a esquerda\n");
-        printf("4 = Diagonal para trás e para a direita\n");
-        scanf("%d", &lado);
-
-        
-        if (lado == 1) {
-            do {
-                printf("Um para frente e um para esquerda\n");
-                b++;
-            } while (b < 5);
-        } else if (lado == 2)
-        {
-            b = 0;
-            do {
-                printf("Um para frente e um para direita\n");
-                b++;
-            } while (b < 5);
-        } else if (lado == 3)
-        {
-            do
-            {
-                b++;    
-                printf("Um para trás e um para a esquerda\n");
-            } while (b < 5);
-            
-        } else
-        {
-            do
-            {
-                b++;
-                printf("Um para trás e um para a direita\n");
-
-            } while (b < 5);
-            
-        }
-        
-        
-        break;
-
-    case 3:
-        printf("Escolha pra qual lado...\n");
-        printf("1 = Diagonal para esquerda\n");
-        printf("2 = Diagonal para direita\n");
-        printf("3 = Esquerda\n");
-        printf("4 = Direita\n");
-        printf("5 = Para frente\n");
-        printf("6 = Para tras\n");
         scanf("%d", &lado);
         printf("\n");
 
-        r = 0;
-        if (lado == 1) {
-            while (r < 8) {
-                printf("Um para frente e um para esquerda\n");
-                r++;
-            }
-        } else if (lado == 2) {
-            while (r < 8) {
-                printf("Um para frente e um para direita\n");
-                r++;
-            }
-        } else if (lado == 3) {
-            while (r < 8) {
-                printf("Esquerda\n");
-                r++;
-            }
-        } else if (lado == 4) {
-            while (r < 8) {
-                printf("Direita\n");
-                r++;
-            }
-        } else if (lado == 5) {
-            while (r < 8) {
-                printf("Para frente\n");
-                r++;
-            }
-        } else if (lado == 6) {
-            while (r < 8) {
-                printf("Para trás\n");
-                r++;
-            }
-        } else {
-            printf("Lado inválido!\n");
-        }
+        if (lado == 1) moverTorre("Cima", CASAS_TORRE);
+        else if (lado == 2) moverTorre("Baixo", CASAS_TORRE);
+        else if (lado == 3) moverTorre("Direita", CASAS_TORRE);
+        else if (lado == 4) moverTorre("Esquerda", CASAS_TORRE);
         break;
-case 4:
-    printf("Escolha pra qual lado...\n");
-    printf("1 = L para frente e para direita\n");
-    printf("2 = L para frente e para esquerda\n");
-    printf("3 = L para trás e para direita\n");
-    printf("4 = L para trás e para esquerda\n");
-    scanf("%d", &lado);
 
-    if (lado == 1) {
-        for (c1 = 1; c1 <= 1; c1++) {
-            printf("Frente\n");
-            printf("Frente\n");
+    case 2: // bispo
+        printf("Escolha o lado:\n");
+        printf("1 = Diagonal frente e esquerda\n");
+        printf("2 = Diagonal frente e direita\n");
+        printf("3 = Diagonal trás e esquerda\n");
+        printf("4 = Diagonal trás e direita\n");
+        scanf("%d", &lado);
+        printf("\n");
 
-            c2 = 0;
-            while (c2 < 1) {
-                printf("Direita\n");
-                c2++;
-                printf("\n");
-            }
-        }
+        if (lado == 1) moverBispoRec("Frente e Esquerda", CASAS_BISPO);
+        else if (lado == 2) moverBispoRec("Frente e Direita", CASAS_BISPO);
+        else if (lado == 3) moverBispoRec("Trás e Esquerda", CASAS_BISPO);
+        else if (lado == 4) moverBispoRec("Trás e Direita", CASAS_BISPO);
 
-    } else if (lado == 2) {
-        for (c1 = 1; c1 <= 1; c1++) {
-            printf("Frente\n");
-            printf("Frente\n");
+        moverBispoComLoops(); // loops aninhados
+        break;
 
-            c2 = 0;
-            while (c2 < 1) {
-                printf("Esquerda\n");
-                c2++;
-                printf("\n");
-            }
-        }
+    case 3: // rainha
+        printf("Escolha o lado:\n");
+        printf("1 = Cima\n");
+        printf("2 = Baixo\n");
+        printf("3 = Direita\n");
+        printf("4 = Esquerda\n");
+        printf("5 = Diagonal frente esquerda\n");
+        printf("6 = Diagonal para Direita\n");
+        scanf("%d", &lado);
+        printf("\n");
 
-    } else if (lado == 3) {
-        for (c1 = 1; c1 <= 1; c1++) {
-            printf("Trás\n");
-            printf("Trás\n");
+        if (lado == 1) moverRainha("Cima", CASAS_RAINHA);
+        else if (lado == 2) moverRainha("Baixo", CASAS_RAINHA);
+        else if (lado == 3) moverRainha("Direita", CASAS_RAINHA);
+        else if (lado == 4) moverRainha("Esquerda", CASAS_RAINHA);
+        else if (lado == 5) moverRainha("Frente e Esquerda", CASAS_RAINHA);
+        else if (lado == 6) moverRainha("Frente e Direita", CASAS_RAINHA);
+        break;
 
-            c2 = 0;
-            while (c2 < 1) {
-                printf("Direita\n");
-                c2++;
-                printf("\n");
-            }
-        }
+    case 4: // cavalo
+        moverCavalo();
+        break;
 
-    } else if (lado == 4) {
-        for (c1 = 1; c1 <= 1; c1++) {
-            printf("Trás\n");
-            printf("Trás\n");
-
-            c2 = 0;
-            while (c2 < 1) {
-                printf("Esquerda\n");
-                c2++;
-                printf("\n");
-            }
-        }
-    } else{
-
-            printf("Lado invalido\n");
-    }
-
-    break;
-   
-            
-        
     default:
-        printf("Peça não implementada.\n");
-        break;
+        printf("Peça inválida!\n");
     }
 
     return 0;
-} 
+}
